38cb5b0032b035b5cded4a71a16dbea9eeaae924
Christian Adam <christian.g.adam@gmail.com>
2019-12-10 00:51:23 -0300

    Do not lazy initialize the footer

--- src/components/sections/Footer/index.js
+++ src/components/sections/Footer/index.js
@@ -1,14 +1,2 @@
-import React, { lazy } from 'React';
-import LazyInView from '../../utils/LazyInView';
-
-const Footer = lazy(() => import('./Footer'));
-
-const LazyFooter = () => {
-  return (
-    <LazyInView>
-      <Footer></Footer>
-    </LazyInView>
-  );
-}
-
-export default LazyFooter;
+import Footer from './Footer';
+export default Footer;
